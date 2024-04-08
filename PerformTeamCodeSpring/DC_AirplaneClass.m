bea95380c60bbb97e8e98db622e93ed17708ed56
DaniloTom <148171407+DaniloTom@users.noreply.github.com>
2024-04-08 12:30:14 -0700

    Added Sam and Danilo's Workspace

---
+++ PerformTeamCodeSpring/DC_AirplaneClass.m
@@ -0,0 +1,14 @@
+% Class to represent airplane configuration and state of airplane
+
+classdef DC_AirplaneClass
+    properties
+        % 
+
+    end
+    methods
+        function obj = DC_AirplaneClass()
+            % Constructor
+        end
+    end
+
+end
